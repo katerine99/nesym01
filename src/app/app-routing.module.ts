1d4ec327246a23fd1a3f5038b975a75ac11c5f09
katherineM99 <taniamm_014@hotmail.com>
2023-09-22 11:18:35 -0500

    2.0.version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PrincipalComponent,
+    children: [
+      { path: '', component: DashboardComponent, },
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+     ]
+  },
+
+{path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
